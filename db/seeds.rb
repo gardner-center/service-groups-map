228b96f0333189e638c68b3ec61c7f58996f3365
JBB <jay.borenstein@stanford.edu>
2010-07-06 09:40:17 -0700

    experimenting with table-based layout scheme

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,9 @@ service_groups = ServiceGroup.create([{ :name => 'John Gardner Center', :address
 
 ServicePerson.create([{ :service_group_id => service_groups[0].id, :first_name => 'Laura',:last_name => 'Ma', :email => 'nowhere@nowhere.com', :phone => '650.723.3682'},{:service_group_id => service_groups[0].id, :first_name => 'Amy',:last_name => 'Gerstein', :email => 'nowhere@nowhere.com', :phone => '650.723.3682'}])  
 
-Program.create([{:name => 'soccer', :description => 'running around in packs', :start_date => '2010-07-01', :end_date => '2010-07-01', :start_time => 16, :end_time => 20, :repeats => 1, :range => '2010-07-01', :age_min => 8, :age_max => 14, :cost => 0, :address1 => '505 Lausen Mall',:city => 'Stanford', :state => 'CA', :zipcode => '94305-3083'},{:name => 'Arts and crafts', :description => 'paints and drawing', :start_date => '2010-07-08', :end_date => '2010-07-08', :start_time => 11, :end_time => 13, :repeats => 2, :range => '2010-11-10', :age_min => 8, :age_max => 14, :cost => 0, :address1 => '505 Lausen Mall',:city => 'Stanford', :state => 'CA', :zipcode => '94305-3083'}])
+Zip.create([{:code => '94305-3083', :city => 'Stanford', :state => 'CA', :lat => 37.426394, :lon => -122.173745}])
+
+Program.create([{:name => 'soccer', :description => 'running around in packs', :start_date => '2010-07-01', :end_date => '2010-07-01', :start_time => 16, :end_time => 20, :repeats => 1, :range => '2010-07-01', :age_min => 8, :age_max => 14, :cost => 0, :address1 => '505 Lausen Mall',:city => 'Stanford', :state => 'CA', :zipcode => '94305-3083', :zip_id => 1},{:name => 'Arts and crafts', :description => 'paints and drawing', :start_date => '2010-07-08', :end_date => '2010-07-08', :start_time => 11, :end_time => 13, :repeats => 2, :range => '2010-11-10', :age_min => 8, :age_max => 14, :cost => 0, :address1 => '505 Lausen Mall',:city => 'Stanford', :state => 'CA', :zipcode => '94305-3083', :zip_id => 1}])
 
 Repeat.create([{:name => 'Does not repeat'}, {:name => 'Daily'}, {:name => 'Mon-Fri'}, {:name => 'Mon/Wed/Fri'}, {:name => 'Tues/Thurs'}, {:name => 'Weekly'}, {:name => 'Monthly'}, {:name => 'Yearly'}])
 
