224d3a3de13f12c27d1d55fcf68950de3e850e95
jaybrianb <jayb@stanfordalumni.org>
2010-08-18 13:53:52 -0700

    seeds.rb throw away modification

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,15 +5,17 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
-Repeat.create([{:name => 'Does not repeat'}, {:name => 'Daily'}, {:name => 'Mon-Fri'}, {:name => 'Mon/Wed/Fri'}, {:name => 'Tues/Thurs'}, {:name => 'Weekly'}, {:name => 'Monthly'}, {:name => 'Yearly'}])
+#Repeat.create([{:name => 'Does not repeat'}, {:name => 'Daily'}, {:name => 'Mon-Fri'}, {:name => 'Mon/Wed/Fri'}, {:name => 'Tues/Thurs'}, {:name => 'Weekly'}, {:name => 'Monthly'}, {:name => 'Yearly'}])
 
-Style.create([{:name => 'Drop-in'}, {:name => 'Set schedule'}])
+#Style.create([{:name => 'Drop-in'}, {:name => 'Set schedule'}])
 
-Category.create([{:name => 'Sports'}, {:name => 'Faith-based'}, {:name => 'Academic'}, {:name => 'Youth leadership'}, {:name => 'Community service'}, {:name => 'Enrichment'}, {:name => 'Arts & music'}, {:name => 'Vocational'}, {:name => 'Mentorship'}])
+#Category.create([{:name => 'Sports'}, {:name => 'Faith-based'}, {:name => 'Academic'}, {:name => 'Youth leadership'}, {:name => 'Community service'}, {:name => 'Enrichment'}, {:name => 'Arts & music'}, {:name => 'Vocational'}, {:name => 'Mentorship'}])
 
-service_groups = ServiceGroup.create([{ :name => 'John Gardner Center', :address1 => '505 Lausen Mall',:city => 'Stanford', :state => 'CA', :zip => '94305-3083', :phone => '650.723.1137', :inception_date => '2000-09-01'},{:name => 'YDA at the John Gardner Center', :address1 => '505 Lausen Mall',:city => 'Stanford', :state => 'CA', :zip => '94305-3083', :phone => '650.723.1137', :inception_date => '2000-09-01'}])  
 
-ServicePerson.create([{ :service_group_id => service_groups[0].id, :first_name => 'Laura',:last_name => 'Ma', :email => 'nowhere@nowhere.com', :phone => '650.723.3682'},{:service_group_id => service_groups[0].id, :first_name => 'Amy',:last_name => 'Gerstein', :email => 'nowhere@nowhere.com', :phone => '650.723.3682'}])  
+
+#service_groups = ServiceGroup.create([{ :name => 'John Gardner Center', :address1 => '505 Lausen Mall',:city => 'Stanford', :state => 'CA', :zip => '94305-3083', :phone => '650.723.1137', :inception_date => '2000-09-01'},{:name => 'YDA at the John Gardner Center', :address1 => '505 Lausen Mall',:city => 'Stanford', :state => 'CA', :zip => '94305-3083', :phone => '650.723.1137', :inception_date => '2000-09-01'}])  
+
+#ServicePerson.create([{ :service_group_id => service_groups[0].id, :first_name => 'Laura',:last_name => 'Ma', :email => 'nowhere@nowhere.com', :phone => '650.723.3682'},{:service_group_id => service_groups[0].id, :first_name => 'Amy',:last_name => 'Gerstein', :email => 'nowhere@nowhere.com', :phone => '650.723.3682'}])  
 
 #Zip.create([{:code => '94305-3083', :city => 'Stanford', :state => 'CA', :lat => 37.426394, :lon => -122.173745}])
 
